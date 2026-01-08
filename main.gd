b52cd632da0e78073d82c8c89b206fa28e2956ce
Zeekoo-11 <jacobally03@gmail.com>
2026-01-08 20:05:15 +1100

    7.1
    Finished HUD, fixed new game signal from 5.1

--- main.gd
+++ main.gd
@@ -15,13 +15,15 @@ func _process(delta: float) -> void:
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 func new_game():
+	get_tree().call_group("mobs", "queue_free")
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
-
-func _on_player_hit() -> void:
-	pass # Replace with function body.
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	
 
 
 func _on_mob_timer_timeout() -> void:
@@ -39,6 +41,7 @@ func _on_mob_timer_timeout() -> void:
 
 func _on_score_timer_timeout() -> void:
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_start_timer_timeout() -> void:
