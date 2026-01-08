9fa92a2eefd03e8d50cdb76d10e7d08915d1980a
Zeekoo-11 <jacobally03@gmail.com>
2026-01-08 20:50:52 +1100

    PookieTown Variation
    Changed mob icons, background, fonts, mob spawn rate. No audio, no save

--- main.gd
+++ main.gd
@@ -19,6 +19,7 @@ func game_over():
 func new_game():
 	get_tree().call_group("mobs", "queue_free")
 	score = 0
+	$MobTimer.wait_time = 0.5
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
 	$HUD.update_score(score)
@@ -34,9 +35,17 @@ func _on_mob_timer_timeout() -> void:
 	var direction = mob_spawn_location.rotation + PI / 2
 	direction += randf_range(-PI/4, PI/4)
 	mob.rotation = direction
-	var velocity = Vector2(randf_range(150.0, 250.0), 0)
+	var velocity = Vector2(randf_range(200.0, 350.0), 0)
 	mob.linear_velocity = velocity.rotated(direction)
 	add_child(mob)
+	if score == 10:
+		$MobTimer.wait_time = 0.4
+	elif score == 15:
+		$MobTimer.wait_time = 0.35
+	elif score == 20:
+		$MobTimer.wait_time = 0.3
+	elif score == 25:
+		$MobTimer.wait_time = 0.25
 
 
 func _on_score_timer_timeout() -> void:
