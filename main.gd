3541933b790a67557bacb878282aaa8f1eb8b5dc
Zeekoo-11 <jacobally03@gmail.com>
2026-02-16 16:21:52 +1100

    Added pause menu
    - Volume/resume/quit

--- main.gd
+++ main.gd
@@ -5,6 +5,9 @@ const SAVE_FILE_PATH = "user://savedata.save"
 var score = 0
 var high_score = 0
 
+@onready var pause_menu = $PauseMenu
+var paused = false
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,7 +18,17 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("Pause"):
+		pauseMenu()
+		
+func pauseMenu():
+	if paused:
+		pause_menu.hide()
+		Engine.time_scale = 1
+	else:
+		pause_menu.show()
+		Engine.time_scale = 0
+	paused = !paused
 
 func game_over():
 	$ScoreTimer.stop()
